e9b12de7b7f07ef0d5024847df078e4ae3140801
Chris Worman <chrisworman@gmail.com>
2017-02-25 13:26:32 -0600

    Camera scrolling

---
+++ Assets/Scripts/CameraScrollBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScrollBehavior : MonoBehaviour
+{
+    const float InitialSpeed = 2.0f;
+    float Speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        Speed = InitialSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scroll();
+    }
+
+    void Scroll()
+    {
+        transform.position += transform.up * Speed * Time.deltaTime;
+    }
+}
